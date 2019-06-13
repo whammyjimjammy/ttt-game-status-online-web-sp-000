d57a6af31199cf03bbe0f0cf778926559ec31e06
Jake ORiley <ti1795sv@gmail.com>
2019-06-13 13:28:48 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -30,8 +30,10 @@ def won?(board)
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
       return win_combination
     end
+
     else
-  return false
+      return false
+    end
 end
 
 
