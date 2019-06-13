f62e57b5510f67c34340c41f5ec0d2c50caddce2
Jake ORiley <ti1795sv@gmail.com>
2019-06-13 15:34:02 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,11 +15,63 @@ WIN_COMBINATIONS = [
   [2,4,6]# ETC, an array for each win combination
 ]
 
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
+#board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
 
-def won(board)
-  WIN_COMBINATIONS.each do |row_array|
-    row_array.each do |array_spaces|
-      if board[array_spaces[0]] == "X" && board[array_spaces[1]] == "X" && board[array_spaces[2]] == "X"
-  "X won in the top row"
+def won?(board)
+  WIN_COMBINATIONS.each {|win_combo|
+    index_0 = win_combo[0]
+    index_1 = win_combo[1]
+    index_2 = win_combo[2]
+
+    position_1 = board[index_0]
+    position_2 = board[index_1]
+    position_3 = board[index_2]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combo
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combo
+    end
+  }
+  return false
+end
+
+def full?(board)
+  board.all?{|token| token == "X" || token == "O"}
+end
+
+# def full?(board)
+#   board.all? do |i|
+#     i == "X" || i == "0"
+#   end
+# end
+
+def draw?(board)
+  if !won?(board) && full?(board)
+    return true
+  else
+    return false
+  end
+end
+
+def over?(board)
+  if won?(board) || draw?(board) || full?(board)
+    return true
+  else
+    return false
+  end
+end
+
+def winner(board)
+  index = []
+  index = won?(board)
+  if index == false
+    return nil
+  else
+    if board[index[0]] == "X"
+      return "X"
+    else
+      return "O"
+    end
+  end
 end
