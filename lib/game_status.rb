cba37b2b4acdc81ecfded83af76ce8a1e27e8af5
Jake ORiley <ti1795sv@gmail.com>
2019-06-13 12:58:59 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,10 +19,22 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
-    win_combination do |win_index|
-     if board[win_index[0]] == "X" && board[win_index[1]] == "X" && board[win_index[2]] == "X"
-       return win_combination
-     else
-       false
-     end
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combination
+    else
+      false
+#    win_combination do |win_index|
+#     if board[win_index[0]] == "X" && board[win_index[1]] == "X" && board[win_index[2]] == "X"
+#       return win_combination
+#     else
+#       false
+#     end
 end
